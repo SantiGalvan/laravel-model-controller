c037a2fbcc65e203084d1666e50d175f3f4bf81b
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-12 22:38:55 +0100

    created main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{env('APP_NAME')}} | @yield('title')</title>
+
+    @vite('resources/js/app.js')
+
+    {{-- CDNS --}}
+    @yield('cdns')
 </head>
 <body>
+
+    {{-- Header --}}
+    @include('includes.header')
+
+
+    {{-- Main --}}
+    @yield('main-content')
+
+
+    {{-- Footer --}}
+    @yield('footer')
     
 </body>
 </html>
